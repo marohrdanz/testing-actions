7419484233b74b0d970ec3caefa6c3b53e3eaf1d
Mary Rohrdanz <marohrdanz@mdanderson.org>
2023-02-28 17:27:53 -0600

    Add version file

---
+++ CompatibilityManager.js
@@ -0,0 +1,6 @@
+// CURRENT VERSION NUMBER
+CM.version = "2.23.1";	// Please increment rightmost digit for each interim version
+CM.versionCheckUrl = "https://bioinformatics.mdanderson.org/versioncheck/NGCHM/";
+CM.viewerAppUrl = "https://bioinformatics.mdanderson.org/ngchm/ZipAppDownload";
+CM.classOrderStr = ".classifications_order";
+
